4de5ee71809086aaf6033860f6f1279d58e236a6
Maksymilian Kafka <m.kafka00@gmail.com>
2025-10-19 21:02:35 +0200

    Project Setup
    CMake file (GLFW, GLM, GLAD)

---
+++ Source/main.cpp
@@ -0,0 +1,56 @@
+﻿#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+int main()
+{
+    if (!glfwInit())
+    {
+        std::cerr << "Failed to initialize GLFW\n";
+        return -1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "MyGameEngine", nullptr, nullptr);
+    if (window == nullptr)
+    {
+        std::cerr << "Failed to create GLFW window\n";
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGL())
+    {
+        std::cerr << "Failed to initialize GLAD\n";
+        return -1;
+    }
+
+    glViewport(0, 0, 800, 600);
+
+    std::cout << "Hello from C++23!\n";
+    std::cout << "Initialized OpenGL Version: " << glGetString(GL_VERSION) << "\n";
+
+    while (!glfwWindowShouldClose(window))
+    {
+        if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+            glfwSetWindowShouldClose(window, true);
+
+        glClearColor(0.1f, 0.1f, 0.15f, 1.0f); // Dark grey
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
+}
