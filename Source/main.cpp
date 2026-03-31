bba3a596223f2b9f675eeed07e8f49342582d31b
Maksymilian Kafka <m.kafka00@gmail.com>
2026-03-31 23:19:53 +0200

    Removed non related code

--- Source/main.cpp
+++ Source/main.cpp
@@ -148,36 +148,6 @@ int main()
 		glfwSwapBuffers( window );
 
 		glfwPollEvents();
-
-
-		/*
-		glfwPollEvents();
-
-		ImGui_ImplOpenGL3_NewFrame();
-		ImGui_ImplGlfw_NewFrame();
-		ImGui::NewFrame();
-
-		ImGui::DockSpaceOverViewport();
-		ImGui::ShowDemoWindow();
-		ImGui::Render();
-
-		int display_w, display_h;
-		glfwGetFramebufferSize(window, &display_w, &display_h);
-		glViewport(0, 0, display_w, display_h);
-		glClearColor(0.1f, 0.1f, 0.15f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
-
-		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-
-		if ( io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable ) {
-		    GLFWwindow *backup_current_context = glfwGetCurrentContext();
-		    ImGui::UpdatePlatformWindows();
-		    ImGui::RenderPlatformWindowsDefault();
-		    glfwMakeContextCurrent(backup_current_context);
-		}
-
-		glfwSwapBuffers(window);
-		*/
 	}
 
 	ImGui_ImplOpenGL3_Shutdown();
